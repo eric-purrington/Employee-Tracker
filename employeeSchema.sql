621c30cb47685297d6c9a431f64da065e736fa71
Seth Purr <eric.purrington@gmail.com>
2020-05-19 21:52:54 -0700

    fixed schema

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
   department_id INT NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
